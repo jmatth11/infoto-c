b992abd1f730a2fd94eb259ebce4e4e13191e312
Joshua Matthews <jmatth93@gmail.com>
2021-03-01 00:36:34 -0600

    added some todo comments

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 #include "exif.h"
 #include "jpeg_handler.h"
 
+// TODO
+// - pull json lib to parse config file.
+// - iterate over TAG names to pull info out.
+// - create border around original image.
+// - add text with info to newly bordered image.
+//
+// extra
+// - explore pulling EXIF data out of PNG files.
+
 int main(int argc, const char *argv[]) {
     if (argc < 2) {
         printf("please supply a jpeg file.\n");
