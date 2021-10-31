ac5e674eaaf7364e7bbca8f91b95aa3254f680ef
Joshua Matthews <jmatth93@gmail.com>
2021-10-31 12:05:44 -0500

    added lib dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/jmatth11/array_template deps/array_template
 git clone https://github.com/cesanta/frozen deps/frozen
 
+# the libs I used. newer versions are probably good too
+sudo apt-get install -y libexif12 libjpeg8 libfreetype6
+
