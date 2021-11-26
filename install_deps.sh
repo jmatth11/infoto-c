742094f60803c2a75195399b771b77981bf6f588
Joshua Matthews <jmatth93@gmail.com>
2021-11-25 20:16:52 -0600

    Update install_deps.sh
    updated to correct packages to install to build

--- install_deps.sh
+++ install_deps.sh
@@ -2,5 +2,5 @@ git clone https://github.com/jmatth11/array_template deps/array_template
 git clone https://github.com/cesanta/frozen deps/frozen
 
 # the libs I used. newer versions are probably good too
-sudo apt-get install -y libexif12 libjpeg8 libfreetype6
+sudo apt-get install -y libexif-dev libexif12 libjpeg8-dev libfreetype6-dev
 
