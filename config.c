d32babb82fcd9434d4b9e3011948e69a2432e777
Joshua Matthews <jmatth93@gmail.com>
2023-09-29 14:31:41 -0500

    fixed typo

--- config.c
+++ config.c
@@ -6,7 +6,7 @@
 void infoto_init_config(config *cfg) { init_metadata_array(&cfg->metadata, 1); }
 
 void infoto_free_config(config *cfg) {
-  free(cfg->img);
+  free(cfg->target);
   free(cfg->font.ttf_file);
   free_metadata_array(&cfg->metadata);
 }
@@ -17,7 +17,7 @@ void infoto_print_config(const config *cfg) {
     return;
   }
   printf("config structure: {\n");
-  printf("img: \"%s\"\n", cfg->img);
+  printf("img: \"%s\"\n", cfg->target);
   printf("font: {\n");
   if (cfg->font.ttf_file == NULL) {
     printf("config font ttf_file was null\n");
